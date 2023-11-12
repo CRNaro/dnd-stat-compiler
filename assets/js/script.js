aa97644793339d05413a348c048d79be95fe170a
michael <mikek222@gmail.com>
2023-11-12 16:29:48 -0500

    Created global variables for localRace and localClass

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,51 @@
 // This is our javascript file for our application
 const getUserClass = document.getElementById("class-select")
 const getUserRace = document.getElementById("race-select")
-
 const formEl = document.getElementById("select-form")
-
-function getApi() {
-    let Url = "https://www.dnd5eapi.co/api/ability-scores/con"
-
-    fetch(Url)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-    console.log(data)}
-)}
-
-getApi()
-
+let classApiResults, raceApiResults;
 
 function getClass(job) {
-  
+
     let classUrl = "https://www.dnd5eapi.co/api/classes/" + job;
     console.log(classUrl)
 
+    fetch(classUrl)
+      .then(function(response) {
+        if (!response.ok){
+            throw response.json();
+        }
+
+        return response.json()
+      })
+
+      .then(function(localClass) {
+        // console.log(localClass)
+       classApiResults = localClass
+       console.log(localClass)
+      })
 }
 
 function getRace(race) {
     let raceUrl = "https://www.dnd5eapi.co/api/races/" + race;
     console.log(raceUrl)
+
+    fetch(raceUrl)
+      .then(function(response) {
+        if (!response.ok){
+            throw response.json();
+        }
+
+        return response.json()
+      })
+
+      .then(function(localRace) {
+        // console.log(localRace)
+        raceApiResults = localRace
+        console.log(localRace)
+      })
 }
 
-formEl.addEventListener("submit", function() {
+formEl.addEventListener("submit", function (event) {
     event.preventDefault()
     const userClass = getUserClass.value
     const userRace = getUserRace.value
