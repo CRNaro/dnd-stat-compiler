aa864cdd822fe4bfbae4b7ca0e5b66848c568ffd
Gisela <giselamata27@gmail.com>
2023-11-12 16:54:36 -0500

    Adding new content

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,31 @@ function getApi() {
     .then(function(data) {
     console.log(data)}
 )}
+// Adding in quote API as a test!!!!!!!!!!!
+function getPoem () {
+    let poemUrl = "https://poetrydb.org/random";
+    console.log(poemUrl)
+    fetch(poemUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+    console.log(data)
+    let poemDisplay = document.getElementById("quote-box");
+    poemDisplay.innerHTML = data[0].lines.join("<br>");
+    });
+}
+getPoem() // THIS IS A TEST!!! Line 15 - 29 are test lines!!!
+
+// Call the fetchQuote function to get and display a quote
+fetchQuote();
+
+// Display inside the stats
+function displayStats(variable1, variable2, variable3) {
+    document.getElementById("variable1").textContent = variable1;
+    document.getElementById("variable2").textContent = variable2;
+    document.getElementById("variable3").textContent = variable3;
+}
 
 getApi()
 
@@ -27,6 +52,3 @@ function getRace() {
     let raceUrl = "https://www.dnd5eapi.co/api/races/" + getRace
     console.log(raceUrl)
 }
-
-
-
