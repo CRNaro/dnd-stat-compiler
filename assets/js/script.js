1e419aacf9f94befbee346ceb16effe645eb204f
Gisela <giselamata27@gmail.com>
2023-11-13 18:14:26 -0500

    Merging gmfun to main branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,12 @@
 // This is our javascript file for our application
 const getUserClass = document.getElementById("class-select")
 const getUserRace = document.getElementById("race-select")
+const formEl = document.getElementById("select-form")
+let classApiResults, raceApiResults;
 
-function getApi() {
-    let Url = "https://www.dnd5eapi.co/api/ability-scores/con"
+function getClass(job) {
 
-    fetch(Url)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-    console.log(data)}
-)}
-
-getApi()
-
-
-function getClass() {
-  
-    let classUrl = "https://www.dnd5eapi.co/api/classes/" + getClass;
+    let classUrl = "https://www.dnd5eapi.co/api/classes/" + job;
     console.log(classUrl)
 
     fetch(classUrl)
@@ -40,7 +28,30 @@ function getClass() {
 function getRace(race) {
     let raceUrl = "https://www.dnd5eapi.co/api/races/" + race;
     console.log(raceUrl)
+
+    fetch(raceUrl)
+      .then(function(response) {
+        if (!response.ok){
+            throw response.json();
+        }
+
+        return response.json()
+      })
+
+      .then(function(localRace) {
+        // console.log(localRace)
+        raceApiResults = localRace
+        console.log(localRace)
+      })
 }
 
+formEl.addEventListener("submit", function (event) {
+    event.preventDefault()
+    const userClass = getUserClass.value
+    const userRace = getUserRace.value
+    console.log(userClass, userRace)
+    getClass(userClass)
+    getRace(userRace)
+})
 
 
