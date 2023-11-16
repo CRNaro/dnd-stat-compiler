11e64ba7e9d0d36f61557399ec56282033ec8e37
Gisela <giselamata27@gmail.com>
2023-11-15 19:02:03 -0500

    Moved js localstorage file

---
+++ assets/js/local.js
@@ -0,0 +1,21 @@
+// JS FOR OUR SAVE TO LOCAL STORAGE FUNCTIONALITY
+document.addEventListener("DOMContentLoaded", function() {
+    // Retrieve the character stats from local storage
+    const displayedClass = localStorage.getItem("userClass"); //userClass?
+    const displayedRace = localStorage.getItem("userRace"); //userRace?
+    const displayedPoem = localStorage.getItem("userPoem"); //userPoem?
+    
+    // Display the character stats on the page
+    document.getElementById("displayClass").innerHTML = `Class: ${displayedClass}`;
+    document.getElementById("displayRace").innerHTML = `Race: ${displayedRace}`;
+    document.getElementById("displayPoem").innerHTML = `Poem: ${displayedPoem}`;
+    console.log()
+    // Display poem on the page
+});
+
+const returnBtn= document.getElementById("return-main")
+
+returnBtn.addEventListener("click", function(event) {
+    console.log("returnBtn")
+    window.location.href = "index.html";
+}); 
